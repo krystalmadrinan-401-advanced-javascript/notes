2458d59017dcf5090fd58216bea85fd830f5f8ce
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2020-03-19 23:33:48 -0700

    not finished yet

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('APP EXISTS');
+
+//parse command line arguments
+console.log(process.argv.slice(2));
+
+/* 
+- Requires the library files you will be writing (input, notes)
+- Instantiates an “Input” parser
+- Sends properly parsed input to the Notes library for display
+*/
+
+// const a = require('./lib/input.js');
+// a(process.argv.slice(2));
